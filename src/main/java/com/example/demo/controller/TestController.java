1ec2c5844cca0ed1b53efc20d8bd6e3bb05d38b6
manumura <emmanuel.mura@gmail.com>
2022-06-26 16:57:41 +0700

    refactor authentication

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -26,26 +26,26 @@ public class TestController {
   @GetMapping("/config/topics/{topicName}")
   public ResponseEntity<String> getTopic(@PathVariable String topicName) {
     System.out.println("client2 Test OK " + topicName);
-    return ResponseEntity.ok("client2 test1 OK " + topicName);
+    return ResponseEntity.ok("client2 test OK " + topicName);
   }
 
   @GetMapping("/config/types/{typeId}")
-  public ResponseEntity<String> getConfigType(@RequestHeader(value = "GUAC-Authorization", required = false) String token, @PathVariable Integer typeId) {
+  public ResponseEntity<String> getConfigType(@RequestHeader(value = "Authorization", required = false) String token, @PathVariable Integer typeId) {
     System.out.println("client2 Test OK " + typeId);
-    return ResponseEntity.ok("client2 test2 OK " + typeId + " + token " + token);
+    return ResponseEntity.ok("client2 test OK " + typeId + " + token " + token);
   }
 
   @GetMapping("/config/test/exception")
-  public ResponseEntity<String> getException(@RequestHeader(value = "GUAC-Authorization", required = false) String token) {
+  public ResponseEntity<String> getException(@RequestHeader(value = "Authorization", required = false) String token) {
     System.out.println("client2 Test exception " + token);
     return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Test exception");
   }
 
   @PostMapping("/config/types/{typeId}")
-  public ResponseEntity<String> getConfigType(@RequestHeader(value = "GUAC-Authorization", required = false) String token,
+  public ResponseEntity<String> getConfigType(@RequestHeader(value = "Authorization", required = false) String token,
       @PathVariable Integer typeId,
       @RequestBody TestPayload payload) {
-    System.out.println("POST client1 Test OK " + typeId + " " + payload);
-    return ResponseEntity.ok("client1 test2 OK " + typeId + " + token " + token + " payload " + payload);
+    System.out.println("POST client2 Test OK " + typeId + " " + payload);
+    return ResponseEntity.ok("client2 test OK " + typeId + " + token " + token + " payload " + payload);
   }
 }
